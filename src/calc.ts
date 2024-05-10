51a80527e949b643d270ac377ef10695004e9023
Stephen Blackstone <sblackstone@gmail.com>
2024-05-10 19:52:48 -0400

    Add the ability to specify water in code as a %

--- src/calc.ts
+++ src/calc.ts
@@ -3,24 +3,38 @@
 import { ingredients, IIngredients } from './ingredients';
 
 
+const TARGET_WATER_PCT = 0.68;
+
 const scaleIngredients  = (scalar : number) => {
     const result : { [key:string] : number}= {};
+    result["total"] = 0;
     for (const [ingredient, qty] of Object.entries(ingredients)) {
       result[ingredient] = qty * scalar;
+      result["total"] += qty * scalar;
     }
+    result["water"] = result["total"] * TARGET_WATER_PCT;
     return result;
 }
 
 const outputRecipe = (recipe:IIngredients) => {
+  let total = 0;
+
   for (const [ingredient, qty] of Object.entries(recipe)) {
     const roundedQty : number = Math.round(qty*1000) / 1000.0;
     console.log(`${ingredient}\t${roundedQty}`);
   }
+
+  let waterPct  = recipe["water"] / recipe["total"];
+  waterPct  = Math.round(waterPct*1000) / 1000.0;
+
+
+  console.log(`Water percentage = ${waterPct*100}%`);
+
 }
 
 const PIZZAS_PER_RECIPE = 3.0;
 const PIZZAS_TO_MAKE = parseInt(process.argv[2]);
-const scale =  (PIZZAS_TO_MAKE / PIZZAS_PER_RECIPE) + 0.03; 
+const scale =  (PIZZAS_TO_MAKE / PIZZAS_PER_RECIPE) + 0.01; 
 console.log(`Making ${PIZZAS_TO_MAKE} pizzas by scaling the recipe by ${scale*100} %`)
 
 
--- src/ingredients.ts
+++ src/ingredients.ts
@@ -4,7 +4,7 @@ export interface IIngredients { [key:string]: number }
 
 export const ingredients : IIngredients= {
   "flour": 747.14,
-  "water": 508.06,
+//  "water": 508.06,
   "idy": 3.74,
   "salt": 16.81,
 };
